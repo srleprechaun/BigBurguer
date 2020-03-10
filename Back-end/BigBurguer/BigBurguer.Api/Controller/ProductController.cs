7f8abd3ebbce5327907505c4948cfbf5892f9c76
Giovani Martins Pires <5by5.gpires@voeazul.com.br>
2020-03-10 08:53:14 -0300

    Add base project in a new architecture

---
+++ Back-end/BigBurguer/BigBurguer.Api/Controller/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BigBurguer.Api.Controller
+{
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: /api/v1/product
+        [HttpGet]
+        public ActionResult<string> GetAll()
+        {
+            return "test";
+        }
+
+        // POST: /api/v1/product
+        // PUT: /api/v1/product/<id>
+        // DELETE: /api/v1/product/<id>
+    }
+}
