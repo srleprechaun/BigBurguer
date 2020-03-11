262a06659f13dd20adeca55c88d97c0d4e427c9b
Lucas Sandrini Bezerra <lucas.bezerra@programmers.com.br>
2020-03-11 09:07:13 -0300

    Added swagger at the project

--- Back-end/BigBurguer/BigBurguer.Api/Controllers/ProductController.cs
+++ Back-end/BigBurguer/BigBurguer.Api/Controllers/ProductController.cs
@@ -1,15 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BigBurguer.Api.Views;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace BigBurguer.Api.Controller
 {
+    [Route("api/product")]
     [ApiController]
     public class ProductController : ControllerBase
     {
         // GET: /api/v1/product
         [HttpGet]
-        public ActionResult<string> GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return "test";
+            var test = new Product();
+
+            test.ImageUrl = "https://cdn4.bobsfa.com.br/assets/cardapio/produtos/double-cheese.png";
+            test.Name = "Double Cheese";
+            test.Ingredients = "Salt, pepper and milk.";
+            test.Price = 13;
+ 
+            return Ok(test);
         }
 
         // POST: /api/v1/product
--- Back-end/BigBurguer/BigBurguer.Api/Startup.cs
+++ Back-end/BigBurguer/BigBurguer.Api/Startup.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace BigBurguer.Api
 {
@@ -16,24 +13,37 @@ namespace BigBurguer.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BigBurguerAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("./swagger/v1/swagger.json", "BigBurguerAPI");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
