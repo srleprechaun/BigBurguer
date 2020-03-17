ef70ff38216c021e98a0ea502cf39a7a593e5eb7
Lucas Sandrini Bezerra <lucas.bezerra@programmers.com.br>
2020-03-17 17:33:37 -0300

    Installed Entity Framework and Create First Migration

---
+++ Back-end/BigBurguer/BigBurguer.Api/Infrastructure/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace BigBurguer.Api.Infrastructure.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
