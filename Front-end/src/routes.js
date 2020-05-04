9717ae0eef5a56ec9a3c4c6ebd9fbf8656d45ff5
Daniel Lopes <Daniellopes03@hotmail.com>
2020-05-03 22:54:52 -0300

    Sprint 3 Tarefa 56

--- Front-end/src/routes.js
+++ Front-end/src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/home/home';
+import cadastroProduto from './pages/cadastroProduto/cadastroProduto';
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/cadastroProduto" component={cadastroProduto}></Route>
         </Switch>
     </BrowserRouter>
 );
