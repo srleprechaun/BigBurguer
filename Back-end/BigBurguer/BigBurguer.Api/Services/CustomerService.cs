8b3c36c345833bf428280a33961078e94eabc771
Lucas Sandrini <lucas.bezerra@programmers.com.br>
2020-06-22 19:27:22 -0300

    added customer crud

---
+++ Back-end/BigBurguer/BigBurguer.Api/Services/CustomerService.cs
@@ -0,0 +1,82 @@
+﻿using BigBurguer.Api.Infrastructure.Models;
+using BigBurguer.Api.Views;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BigBurguer.Api.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly AppDbContext _context;
+        public CustomerService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Customer> GetAll()
+        {
+            return _context.Customers.ToList();
+        }
+
+        public Customer GetId(int customerId)
+        {
+            return _context.Customers.Find(customerId);
+        }
+
+        public EntityEntry<Customer> CreateCustomer(CustomerViewModel customerModel)
+        {
+            Customer customer = new Customer()
+            {
+                BirthDate = customerModel.BirthDate,
+                Cpf = customerModel.Cpf,
+                Name = customerModel.Name,
+                Password = customerModel.Password
+            };
+
+            var result = _context.Customers.Add(customer);
+
+            if (result != null)
+            {
+                _context.SaveChanges();
+
+                return result;
+            }
+            return null;
+        }
+
+        public EntityEntry<Customer> EditCustomer(int customerId, CustomerViewModel customerModel)
+        {
+            var customer = _context.Customers.Find(customerId);
+
+            if (customer != null)
+            {
+                customer.Name = customerModel.Name;
+                customer.BirthDate = customerModel.BirthDate;
+                customer.Cpf = customerModel.Cpf;
+                customer.Password = customerModel.Password;
+
+                var result = _context.Customers.Update(customer);
+
+                _context.SaveChanges();
+                return result;
+            }
+            return null;
+        }
+
+        public EntityEntry<Customer> DeleteCustomer(int productId)
+        {
+            var customer = _context.Customers.Where(i => i.Id == productId).FirstOrDefault();
+
+            if (customer != null)
+            {
+                var result = _context.Customers.Remove(customer);
+                _context.SaveChanges();
+
+                return result;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Back-end/BigBurguer/BigBurguer.Api/Services/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿using BigBurguer.Api.Infrastructure.Models;
+using BigBurguer.Api.Views;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections.Generic;
+
+namespace BigBurguer.Api.Services
+{
+    public interface ICustomerService
+    {
+        public List<Customer> GetAll();
+        public Customer GetId(int customerId);
+        public EntityEntry<Customer> CreateCustomer(CustomerViewModel customerModel);   
+        public EntityEntry<Customer> EditCustomer(int id, CustomerViewModel customerModel);
+        public EntityEntry<Customer> DeleteCustomer(int customerId);
+    }
+}
